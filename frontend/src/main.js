332ef8694d6c8983392d928c7960efbe2c0add5b
Ava <cugb_ljl@126.com>
2026-06-08 02:21:06 +0800

    feat: set Element Plus locale to zh-cn for Chinese calendar display

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).use(ElementPlus).use(router).use(createPinia()).mount('#app')
+createApp(App).use(ElementPlus, { locale: zhCn }).use(router).use(createPinia()).mount('#app')
